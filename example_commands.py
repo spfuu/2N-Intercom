1f675b6ede48dc97b5140137acef61f6617b7d30
pfischi <pfischi@gmx.de>
2016-12-04 18:19:51 +0100

    clearer example

--- example_commands.py
+++ example_commands.py
@@ -6,17 +6,23 @@ authentication method and.
 Check your IP-Com settings to get the correct values.
 '''
 
-username = 'testuser'
+username = 'test'
 password = 'testpassword'
-ip = '192.168.0.1'
+ip = '192.168.0.2'
 ssl = True
-auth_type = 2 # 0=None, 1=Basic, 2=Digest
-
+auth_type = 2  # 0=None, 1=Basic, 2=Digest
 
 ip_cam = IPCam(ip, ssl=ssl, auth_type=2, user=username, password=password)
 
-# for the comple list of commands anf their description please take a look to service.py within the EventService class.
+# For the complete list of commands anf their description please take a look
+# to service.py within the EventService class.
+# The repsonse is a requests.response (data as a dictionary).
 
-ip_cam.commands.info()  # basic device information
-ip_cam.commands.status()  # current intercom status
-ip_cam.commands.dial('**618') # dial number
+response = ip_cam.commands.info()  # basic device information
+response.raise_for_status()
+print(response.text)
+
+print(ip_cam.commands.status().text)  # current intercom status
+print(ip_cam.commands.dial('**618').text)  # dial number
+
+#  and many more
