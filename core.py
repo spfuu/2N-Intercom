1173d3966cf968a12da08d02999c3423725c78a4
pfischi <pfischi@gmx.de>
2016-12-07 20:37:10 +0100

    get intercom systemtime to set an initial time value for subscription event

--- core.py
+++ core.py
@@ -1,4 +1,7 @@
+import datetime
+import json
 import logging
+from error import raise_for_intercom_error
 from service import EventService, CommandService
 
 log = logging.getLogger(__name__)
@@ -12,5 +15,22 @@ class IPCam(object):
         self.auth_type = int(auth_type)  # 0: none, 1: basic, 2: digest
         self.ssl = ssl
         self.commands = CommandService(self)
-        self.event = EventService(self)
+
+        # get system time
+        response = self.commands.status()
+
+        # check for critical connection errors
+        response.raise_for_status()
+
+        # check for internal errors
+        raise_for_intercom_error(response.text)
+
+        status = json.loads(response.text)
+
+        if 'systemTime' not in status['result']:
+            raise Exception("Unknown intercom response: 'systemTime' missing.")
+
+        # we just want to have current events
+        event_register_time = datetime.datetime.utcfromtimestamp(status['result']['systemTime'])
+        self.event = EventService(self, event_register_time)
 
